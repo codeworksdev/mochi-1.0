53901afb6f866d13b89f4f23d706c3d5de2551fe
CODEWORKS <support@codeworksnyc.com>
2016-12-06 12:57:05 -0500

    v1.2.3

--- dist/mochi/js/mochi.js
+++ dist/mochi/js/mochi.js
@@ -1,5 +1,5 @@
 /*!
- * Mochi v1.2.2 (https://github.com/codeworksdev/mochi)
+ * Mochi v1.2.3 (https://github.com/codeworksdev/mochi)
  * Copyright (c) 2014-2016 CODEWORKS <support@codeworksnyc.com>
  * Licensed under the MIT license
  */
@@ -20,7 +20,7 @@ function Mochi()
 {
     this
       ._conf()
-      ._func()
+      ._observer()
       ._html()
       ._init();
 };
@@ -49,14 +49,6 @@ Mochi.prototype =
         this.container = this.body.find('#container');
         this.content   = this.body.find('#content');
         this.filename  = /([^\\\/]+?)(?:\.\w+)?$/.test(this.href) ? RegExp.$1 : this.o.DEF_FILENAME;
-
-        return this
-    },
-
-    _func : function()
-    {
-        this.GET = {};
-        this.SET = {};
 
         return this
     },
@@ -148,47 +140,42 @@ Mochi.prototype =
         return this
     },
 
-    vClickName     : function(){return this.html.hasClass('no-touchevents')?this.o.CLICK_D:this.o.CLICK_M},
-    onClick        : function(e,f){if(_.isFunction(f))(_.isElement(e)?$(e):e).on(this.vClickName(),f);return this},
-    getPage        : function(p){return this._getState('Page',p)},
-    getView        : function(p){return this._getState('View',p)},
-    setPage        : function(n,f){return this._setState('Page',n,f)},
-    setView        : function(n,f){return this._setState('View',n,f)},
-    onPageChange   : function(old_p){return this._onStateChange('Page',old_p)},
-    onViewChange   : function(old_v){return this._onStateChange('View',old_v)},
-    loadPage       : function(n){return this._load('Page',n)},
-    loadView       : function(n){return this._load('View',n)},
-    unloadPage     : function(n){return this._unload('Page',n)},
-    unloadView     : function(n){return this._unload('View',n)},
-    do             : function(f){x=this.sanitize_title(f);if(/^\w+$/.test(x))eval('if(_.isFunction(window["'+x+'"]))window["'+x+'"].call(this);');return this},
-    cache          : function(src,w,h){var e=new Image(w,h);e.src=src;return e},
-    sanitize_title : function(s,r){return s.replace(/\W/g,r==null?'_':r)},
-
-    Option : function(k,v)
+    _observer : function()
     {
-        if (
-          arguments.length
-          && /^\w+$/.test(k))
-        {
-            if (arguments.length == 1)
-            {
-                return _.isFunction(this.GET[k])
-                  ? this.GET[k]()
-                  : this.o[k];
-            }
-            else
-            {
-                if (_.isFunction(this.SET[k])) this.SET[k](v);
-                else this.o[k] = v;
-            }
-        }
+        var target   = this.container.get(0),
+            config   = {attributes: true, childList: true, characterData: true},
+            observer = new MutationObserver(
+                function(mutations)
+                {
+                    mutations.forEach(
+                        $m.onMutation
+                        );
+                }
+                );
 
+        observer.observe(target,config);
         return this
     },
 
+    cache          : function(src,w,h){var e=new Image(w,h);e.src=src;return e},
+    do             : function(f){x=this.sanitize_title(f);if(/^\w+$/.test(x))eval('if(_.isFunction(window["'+x+'"]))window["'+x+'"].call(this);');return this},
+    getPage        : function(p){return this._getState('Page',p)},
+    getView        : function(p){return this._getState('View',p)},
+    loadPage       : function(n){return this._load('Page',n)},
+    loadView       : function(n){return this._load('View',n)},
+    onMutation     : function(mutation){if(_.isFunction(window['Mochi_onMutation']))Mochi_onMutation.call(this,mutation)},
+    onPageChange   : function(old_p){return this._onStateChange('Page',old_p)},
+    onViewChange   : function(old_v){return this._onStateChange('View',old_v)},
+    unloadPage     : function(n){return this._unload('Page',n)},
+    unloadView     : function(n){return this._unload('View',n)},
+    sanitize_title : function(s,r){return s.replace(/\W/g,r==null?'_':r)},
+    setPage        : function(n,f){return this._setState('Page',n,f)},
+    setView        : function(n,f){return this._setState('View',n,f)},
+    vClickName     : function(){return this.html.hasClass('no-touchevents')?this.o.CLICK_D:this.o.CLICK_M},
+
     extend : function(k,c,f)
     {
-        eval('this[k] = new c(this)');
+        eval('this[k] = new c(c.mochi=this)');
 
         if (
           f
@@ -196,5 +183,23 @@ Mochi.prototype =
         {
             f.call(this[k])
         }
+    },
+
+    onClick : function(selector,data,handler)
+    {
+        var s,
+            e = this.vClickName(),
+            f = _.isFunction(data),
+            d = f ? {} : data,
+            h = f ? data : handler;
+
+        switch (typeof selector)
+        {
+            case 'string': s = $(selector);                                    break;
+            default      : s = _.isElement(selector) ? $(selector) : selector; break;
+        }
+
+        s.on(e,null,d,h);
+        return this
     }
 };
