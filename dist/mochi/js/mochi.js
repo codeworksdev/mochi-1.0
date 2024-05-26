25f1d31772732bf39fc8d8866867f46cfab60f59
Carlos Bonills <carlos.bonilla@codeworksnyc.com>
2024-05-26 16:37:57 -0400

    Issue #257

--- dist/mochi/js/mochi.js
+++ dist/mochi/js/mochi.js
@@ -1,5 +1,5 @@
 /*!
- * Mochi v1.6.1 (https://github.com/codeworksdev/mochi)
+ * Mochi v1.6.2 (https://github.com/codeworksdev/mochi)
  * Copyright (c) 2014-2019 CODEWORKS <support@codeworksnyc.com>
  * Licensed under the MIT license
  */
